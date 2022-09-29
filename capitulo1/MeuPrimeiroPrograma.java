106ccfd759ed907ef9f5a794962bee86e71f9fd8
JCAmorim23 <juliana.carol.amorim@gmail.com>
2022-09-28 21:02:00 -0300

    Capitulo 1

---
+++ capitulo1/MeuPrimeiroPrograma.java
@@ -0,0 +1,6 @@
+class MeuPrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("O primeiro programa de muitos!");
+   //     System.out.println("O primeiro codigo de muitos escritos em "+args[0]+" pela "+ args[1]+"!");
+    }
+}
---
+++ capitulo1/erroCompilacao.java
@@ -0,0 +1,5 @@
+class ErroCompilaçao{
+    public static void main(String[] args){
+//        System.out.println("sem ponto e virgula")
+    }
+}
